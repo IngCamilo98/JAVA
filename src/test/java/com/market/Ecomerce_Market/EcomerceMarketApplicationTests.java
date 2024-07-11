d4bd7093ba837c46f329e57ba59003c2d52d818c
Camilo <hcamilotorresdiaz@gmail.com>
2024-07-11 10:30:56 -0500

    Herllo world 2

---
+++ src/test/java/com/market/Ecomerce_Market/EcomerceMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.Ecomerce_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
