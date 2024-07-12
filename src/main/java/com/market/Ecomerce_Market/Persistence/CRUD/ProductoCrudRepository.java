ec4cf4ea40958af3cd6936f3c23242f9c53d641b
Camilo <hcamilotorresdiaz@gmail.com>
2024-07-12 10:12:11 -0500

    Query methods por medio del lenguaje JAva genberar consultas SQl mas especificas y retornando valores Optinal para mantener la programacion funcional

--- src/main/java/com/market/Ecomerce_Market/Persistence/CRUD/ProductoCrudRepository.java
+++ src/main/java/com/market/Ecomerce_Market/Persistence/CRUD/ProductoCrudRepository.java
@@ -1,9 +1,15 @@
 package com.market.Ecomerce_Market.Persistence.CRUD;
 
+import com.market.Ecomerce_Market.Persistence.Entity.Categoria;
 import com.market.Ecomerce_Market.Persistence.Entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
 
+    List<Producto> findByIdCategoriaOrderByAsc(int idCategoria);
 
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
--- src/main/java/com/market/Ecomerce_Market/Persistence/ProductoRepository.java
+++ src/main/java/com/market/Ecomerce_Market/Persistence/ProductoRepository.java
@@ -2,13 +2,37 @@ package com.market.Ecomerce_Market.Persistence;
 
 import com.market.Ecomerce_Market.Persistence.CRUD.ProductoCrudRepository;
 import com.market.Ecomerce_Market.Persistence.Entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
+
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
